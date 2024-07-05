fa6ebbd3f8963d569f3200f2dbaa8b5fab819752
Yan Aung Hein <yan@apmhelp.com>
2024-07-06 04:15:43 +0700

    add fake transition

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,44 @@
-import type { Metadata } from "next";
-import font from "next/font/local";
-import { twMerge as twm } from "tailwind-merge";
-import Navbar from "@/components/Navbar";
-import SectionGetInTouch from "@/components/GetInTouch";
-import SmoothScroll from "@/components/SmoothScroll";
-import { sanityClient } from "@/studio/lib/client";
-import { generalQuery } from "@/studio/queries";
-import type { General } from "@/studio/types";
+import type { Metadata } from 'next';
+import font from 'next/font/local';
+import { twMerge as twm } from 'tailwind-merge';
+import Navbar from '@/components/Navbar';
+import SectionGetInTouch from '@/components/GetInTouch';
+import SmoothScroll from '@/components/SmoothScroll';
+import { sanityClient } from '@/studio/lib/client';
+import { generalQuery } from '@/studio/queries';
+import type { General } from '@/studio/types';
 
-import "./globals.css";
+import './globals.css';
 
 const generalSans = font({
   src: [
     {
-      path: "../public/fonts/GeneralSans-Regular.ttf",
-      style: "normal",
-      weight: "400",
+      path: '../public/fonts/GeneralSans-Regular.ttf',
+      style: 'normal',
+      weight: '400',
     },
     {
-      path: "../public/fonts/GeneralSans-Medium.ttf",
-      style: "normal",
-      weight: "500",
+      path: '../public/fonts/GeneralSans-Medium.ttf',
+      style: 'normal',
+      weight: '500',
     },
     {
-      path: "../public/fonts/GeneralSans-Semibold.ttf",
-      style: "normal",
-      weight: "600",
+      path: '../public/fonts/GeneralSans-Semibold.ttf',
+      style: 'normal',
+      weight: '600',
     },
     {
-      path: "../public/fonts/GeneralSans-Bold.ttf",
-      style: "normal",
-      weight: "700",
+      path: '../public/fonts/GeneralSans-Bold.ttf',
+      style: 'normal',
+      weight: '700',
     },
   ],
-  variable: "--font-general-sans",
+  variable: '--font-general-sans',
 });
 
 export const metadata: Metadata = {
-  title: "Xers",
-  description: "We flourish your digital vision into reality.",
+  title: 'Xers',
+  description: 'We flourish your digital vision into reality.',
 };
 
 export default async function RootLayout({
@@ -50,13 +50,11 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={generalSans.variable}>
-      <body className={twm(generalSans.className, "bg-white overflow-hidden")}>
+      <body className={twm(generalSans.className, 'bg-white overflow-hidden')}>
         <SmoothScroll>
-          <>
-            <Navbar generalData={generalData} />
-            <main>{children}</main>
-            <SectionGetInTouch generalData={generalData} />
-          </>
+          <Navbar generalData={generalData} />
+          <main>{children}</main>
+          <SectionGetInTouch generalData={generalData} />
         </SmoothScroll>
       </body>
     </html>
